f6ba30580dbe082f16f604baf241438af5721536
Jay Chetty <jaypaulchetty@gmail.com>
2018-01-07 19:37:49 +0000

    svg animation attempt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const offsetX = 10;
+const offsetY = 10;
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,48 +14,86 @@ class App extends Component {
   sequanceArray(size) {
     return Array.from(Array(size).keys());
   }
-  // createRow(rowNumber) {
-  //   return this.sequanceArray(10).map(colNumber => {
-  //     const size = 20;
-  //     const margin = 2;
-  //     const x = size * colNumber + margin * colNumber;
-  //     const y = size * rowNumber + margin * rowNumber;
-  //     return (
-  //       <rect
-  //         className="test"
-  //         x={x}
-  //         y={y}
-  //         width={size}
-  //         height={size}
-  //         rx="5"
-  //         ry="5"
-  //         fill="#121252"
-  //       />
-  //     );
-  //   });
-  // }
-
-  createSquares(number, color, stroke = "#FFFFFF", strokeOpacity = "0.0") {
-    // return this.sequanceArray(10).map(rowNumber => this.createRow(rowNumber));
+  createFilledSquares(number) {
     return this.sequanceArray(number).map(squareNumber => {
       const size = 20;
-      const margin = 2;
+      const margin = 4;
       const rowNumber = Math.floor(squareNumber / 10);
       const colNumber = Math.floor(squareNumber % 10);
-      const x = size * colNumber + margin * colNumber;
-      const y = size * rowNumber + margin * rowNumber;
+      const x = size * colNumber + margin * colNumber + offsetX;
+      const y = size * rowNumber + margin * rowNumber + offsetY;
+
+      return (
+        <g>
+          <rect
+            key={squareNumber}
+            className={"mover"}
+            x={x}
+            y={y}
+            width={size}
+            height={size}
+            fill="#FFBB33"
+            stroke="#000000"
+            fillOpacity="0.8"
+          />
+          <polygon
+            points={`${x},${y} ${x + 5},${y - 5} ${x + 5},${y + 15} ${x},${y +
+              20}`}
+            fill="#FFBB33"
+            fillOpacity="0.8"
+            stroke="#000000"
+            className={"leftCube"}
+          >
+            <animateTransform
+              attributeName="transform"
+              attributeType="XML"
+              type="scale"
+              from="0.6 1"
+              to="1 1"
+              dur="1s"
+            />
+          </polygon>
+          <polygon
+            points={`${x},${y + 20} ${x + 20},${y + 20} ${x + 25},${y +
+              15} ${x + 5},${y + 15}`}
+            fill="#FFBB33"
+            fillOpacity="0.8"
+            stroke="#000000"
+            className={"leftCube"}
+          >
+            <animateTransform
+              attributeName="transform"
+              attributeType="XML"
+              type="scale"
+              from="0.6 1"
+              to="1 1"
+              dur="1s"
+            />
+          </polygon>
+        </g>
+      );
+    });
+  }
+
+  createSquares() {
+    return this.sequanceArray(100).map(squareNumber => {
+      const size = 20;
+      const margin = 4;
+      const rowNumber = Math.floor(squareNumber / 10);
+      const colNumber = Math.floor(squareNumber % 10);
+      const x = size * colNumber + margin * colNumber + offsetX;
+      const y = size * rowNumber + margin * rowNumber + offsetY;
+
       return (
         <rect
-          className="test"
+          key={squareNumber}
+          className={"standard"}
           x={x}
           y={y}
           width={size}
           height={size}
-          rx="5"
-          ry="5"
-          fill={color}
-          stroke={stroke}
-          strokeOpacity={strokeOpacity}
+          fill="#EEEEEE"
+          stroke="#EEEEEE"
         />
       );
     });
@@ -83,21 +123,14 @@ class App extends Component {
   }
 
   render() {
-    const baseRects = this.createSquares(100, "#EEEEEE");
-    const targetRects = this.createSquares(
-      this.state.target,
-      "#EEEEEE",
-      "#FF99DD",
-      "1.0"
-    );
-    const rects = this.createSquares(this.state.number, "#121252");
+    const baseRects = this.createSquares();
+    const rects = this.createFilledSquares(this.state.number);
 
     // const rects = this.createGrid();
     return (
       <div className="App">
-        <svg className="grid" width="220" height="220">
+        <svg className="grid" width="260" height="260">
           {baseRects}
-          {targetRects}
           {rects}
         </svg>
         <span> Moves:{this.state.moves} </span>
