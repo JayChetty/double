710a49e910cd6651bdc7c39f8c0fc21b98375594
Jay Chetty <jaypaulchetty@gmail.com>
2018-04-17 10:50:20 +0100

    make gems!

--- src/components/Gem.js
+++ src/components/Gem.js
@@ -1,14 +1,51 @@
 import React from "react";
 import "./Gem.css";
 
-export default function Gem({ x, y, size, color }) {
+export default function Gem({ size, color }) {
   const hlfSize = size / 2;
+  const gap = size / 5;
+
+  const outerTop = `${hlfSize} 0`;
+  const outerRight = `${size} ${hlfSize}`;
+  const outerBottom = `${hlfSize} ${size}`;
+  const outerLeft = `0 ${hlfSize}`;
+
+  const innerTop = `${hlfSize} ${gap}`;
+  const innerRight = `${size - gap} ${hlfSize}`;
+  const innerBottom = `${hlfSize} ${size - gap}`;
+  const innerLeft = `${gap} ${hlfSize}`;
+
   return (
     <g>
       <path
-        d={`M ${hlfSize} 0 L ${size} ${hlfSize} L ${hlfSize} ${size} L 0 ${hlfSize} Z`}
+        d={`M ${outerTop} L ${innerTop} L ${innerLeft} L ${outerLeft} Z`}
         fill={color}
         stroke={color}
+        opacity="0.2"
+      />
+      <path
+        d={`M ${outerTop} L ${innerTop} L ${innerRight} L ${outerRight} Z`}
+        fill={color}
+        stroke={color}
+        opacity="0.6"
+      />
+      <path
+        d={`M ${outerBottom} L ${innerBottom} L ${innerRight} L ${outerRight} Z`}
+        fill={color}
+        stroke={color}
+        opacity="0.8"
+      />
+      <path
+        d={`M ${outerBottom} L ${innerBottom} L ${innerLeft} L ${outerLeft} Z`}
+        fill={color}
+        stroke={color}
+        opacity="0.5"
+      />
+      <path
+        d={`M ${innerTop} L ${innerRight} L ${innerBottom} L ${innerLeft} Z`}
+        fill={color}
+        stroke={color}
+        opacity="0.25"
       />
       {/* <rect
         className="gem gem-inner"
